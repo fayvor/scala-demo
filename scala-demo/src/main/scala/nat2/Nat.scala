e54a1f5c47e7de05c75ac68a08daafaf0f6ef722
Fayvor Love <fayvor@gmail.com>
2013-08-17 16:38:48 -0700

    compiling with trivial re-association.  working on composing re-assoc from definition of Addition

--- scala-demo/src/main/scala/nat2/Nat.scala
+++ scala-demo/src/main/scala/nat2/Nat.scala
@@ -12,44 +12,53 @@ object Zero extends Nat
 object NatAdd {
   
     sealed trait ANat {
-	  def flatten: ANat
-	  def reduce: Nat
+	  // def flatten: ANat
+	  // def reduce: Nat
+	}
+    
+	sealed trait AZero extends ANat
+	
+	object AZero extends AZero {
+	  // def flatten: ANat = this
+	  // def reduce = Zero
 	}
 	
 	case class ASucc(n: ANat) extends ANat {
-	  def flatten = ASucc(n.flatten)
-	  def reduce = Succ(n.reduce)
+	  // def flatten = ASucc(n.flatten)
+	  // def reduce = Succ(n.reduce)
 	}
 	
-	object AZero extends ANat {
-	  def flatten: ANat = this
-	  def reduce = Zero
-	}
-	
-	case class Add(left: ANat, right: ANat) extends ANat {
-	  def flatten = right match {
-	    case AZero => left.flatten
-	    case ASucc(n) => ASucc(Add(left, n).flatten)
-	    case _: Add => Add(left, right.flatten)
-	  }
-	  def reduce = this.flatten.reduce
+	case class Add[S <: ANat, T <: ANat](left: S, right: T) extends ANat {
+//	  def flatten = right match {
+//	    case AZero => left.flatten
+//	    case ASucc(n) => ASucc(Add(left, n).flatten)
+//	    case _: Add => Add(left, right.flatten)
+//	  }
+	  // def reduce = this.flatten.reduce
 	}
 	
 	object Add {
-	  def addZero = new EqF[ANat, ANat, Add] {
+	  def addZero = new EqF[ANat, ANat, Add[ANat, AZero]] {
 	    def apply(a: ANat) = Add(a, AZero)
-	    def unapply(a: Add) = a.left
+	    def unapply(a: Add[ANat, AZero]) = a.left
 	  }
 	  
-	  def addSucc = new EqF[ANat, Add, ASucc] {
-	    def apply(a: Add) = ASucc(a.right match { case ASucc(x) => x })
-	    def unapply(a: ASucc) = a match { case ASucc(x: Add) => Add(x.left, ASucc(x.right))}
+	  def addSucc = new EqF[ANat, Add[ANat, ASucc], ASucc] {
+	    def apply(a: Add[ANat, ASucc]) = ASucc(Add(a.left, a.right match { case ASucc(x) => x }))
+	    def unapply(a: ASucc) = a match { case ASucc(x: Add[_, _]) => Add(x.left, ASucc(x.right))}
 	  }
 	}
 	
-	def associateRight(n: Add): Add = n.left match {
-	  case AZero => n
-	  case _: ASucc => n
-	  case x: Add => Add(x.left, Add(x.right, n.right)) // doesn't make use of definition
+	object Associate {
+	  def associateZero = new EqF[ANat, Add[Add[ANat, ANat], AZero], Add[ANat, Add[ANat, AZero]]] {
+	    def apply(a: Add[Add[ANat, ANat], AZero]) = Add(a.left.left, Add(a.left.right, AZero))
+	    def unapply(a: Add[ANat, Add[ANat, AZero]]) = Add(Add(a.left, a.right.left), AZero)
+	  }
 	}
+	
+//	def associateRight(n: Add[Add[_, _], ANat]): Add[ANat, Add[_, _]] = n match {
+//	  case x: Add[ANat, AZero] => Add(Add.addZero.unapply(x)
+//	  case _: ASucc => n
+//	  case x: Add => Add(x.left, Add(x.right, n.right)) // doesn't make use of definition
+//	}
 }
